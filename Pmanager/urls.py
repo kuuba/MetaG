2c07c7409f170e833727079b75d66fd7132687ac
Jaak Simm <jaak.simm@gmail.com>
2014-04-21 12:21:04 +0200

    removed broken url

--- Pmanager/urls.py
+++ Pmanager/urls.py
@@ -11,7 +11,6 @@ urlpatterns = patterns('',
     url(r'^create/$', views.create_project, name='crproject'),
 #    url(r'^upload/(\d+)/$', views.upload_js_file, name='upload_js'),
 #    url(r'^upload/(\d+)/$', views.upload_file, name='upload'),
-    url(r'^upload/(\d+)/$', views.resumable, name='resumable'),
     url(r'^edit/(\d+)/$', views.edit_project, name='edproject'),
     url(r'^del/(\d+)/$', views.del_project, name='delproject'),
 )
